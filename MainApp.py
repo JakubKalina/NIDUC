0c35b36a401f6cfc916445cac5d70faf1ead74ce
Jakub Kalina <jakub.kalina@codeteam.pl>
2019-05-25 18:27:13 +0200

    Poprawienie wysyłania i odbierania danych

--- MainApp.py
+++ MainApp.py
@@ -1,38 +1,34 @@
-from Resources.protocols.GoBackN import GoBackN
-from Resources.Sender import Sender
-from Resources.Receiver import Receiver
-import Resources.Image as ImageLib
-from Resources.Interference import interfere
-import time
+import Image as ImageLib
+from Interference import interfere
+from Sender import Sender
+from Receiver import Receiver
+import numpy
+import threading
 
 # deklaracja potrzebnych zmiennych globalnych
 #       - najlepiej żeby nie były to dane które mają zostać/powinny byc przesłane
 width = 14
 height = 14
 cell_width = 3
-object_size = width * height * cell_width
-
 receiver = Receiver()
 sender = Sender(receiver=receiver)
 receiver.set_sender(sender=sender)
-# SKRYPT GŁÓWNY
 
-# wyświetl obraz przed wysłaniem
+#wysw obraz zwykly
+ImageLib.display_original_image(height, width)
 
-# ImageLib.display_original_image(height, width)
+#zepsuj
+array = interfere(ImageLib.image_to_array(ImageLib.load_image()))
+print("podaj wielkosc ramki")
+Sender.size = int(input())
+print("podaj wielkosc okna")
+Sender.windowSize = int(input())
+receiver.frameSize = Sender.size
+Sender.image = array
+threading.Thread(target=Sender.send_frame_selective(Sender)).start()
+receiver.receiver_frame()
+array = numpy.asarray(receiver.receivedArray)
+array = ImageLib.unflatten_array(array, width, cell_width)
 
-# zakłócanie
-array = ImageLib.image_to_array(ImageLib.load_image())
-array = interfere(array)
-
-#  - - - PRZESYŁANIE - - -
-
-# array = numpy.asarray([1, 2, 3, 4, 5, 6])
-goBackN = GoBackN(sender=sender, receiver=receiver, window_size=50, array_size=object_size)
-goBackN.delay = 0
-goBackN.start_transmission(array)
-
-
-new_array = ImageLib.unflatten_array(array, width, cell_width)
-
-ImageLib.display_image(new_array, height=height, width=width)
+#wysw zepsuty
+ImageLib.display_image(array, height=height, width=width)
