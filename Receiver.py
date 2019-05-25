d19148eaa9626912da7b5f54da5f3b39d8d60660
MateNaw <49524385+MateNaw@users.noreply.github.com>
2019-05-26 00:35:38 +0200

    Dodano prowizoryczne zaklocanie 1 ramki ktore nie dziala oraz protokol Selektywny oraz prowizoryczna funkcje odkodowujaca sume

--- Receiver.py
+++ Receiver.py
@@ -1,3 +1,6 @@
+import random
+import time
+import Interference as interfere
 
 class Receiver:
     receivedArray = []
@@ -5,6 +8,7 @@ class Receiver:
     numberOfFrames = 0
     frameSize = 0;
     numberOfValues = 0
+    xd = 0
 
     def __init__(self):
         pass
@@ -21,20 +25,60 @@ class Receiver:
         print (self.receivedData)
 
         for i in range (0, len(self.receivedData)):
-            for j in range (0, len(self.receivedData[i])):
+            for j in range (0, (len(self.receivedData[i]) - 1)):
                 self.receivedArray.append(self.receivedData[i][j])
         print(self.receivedArray)
 
 
 
     def recieve_frame(self, frame, sequence_number):
+        isGood = False
+        rand = random.randrange(0, 100)
+        #zepsucie obrazu jesli wylosuje wartosc
 
-        #tutaj sprawdza czy suma kontrolna jest OK
-        isGood = True
+            #zostanie dodane jutro
+
+
+
+        sumi = self.count_sum(self, frame)
+
+
+        if sumi == frame[len(frame) - 1]:
+            isGood = True
+        else:
+            isGood = False
+
+
+
 
         if (isGood):
-            #self.receivedData.append(frame)
+            time.sleep(0.2)
+            print("received pocket = " + str(sequence_number))
             self.receivedData[sequence_number] = frame
             return True;
+
         else:
+            time.sleep(0.2)
+            print("you need to resend pocket = " + str(sequence_number))
             return False
+
+
+
+    def count_sum(self, frame):
+        sum = 0
+        for k in range(0, len(frame) - 1):
+            help = frame[k]
+            while help > 0:
+                sum += help % 10
+                help = int(help / 10)
+                pass
+        sum = sum % 10
+        sum = 10 - sum
+        return sum
+
+
+    def interfere(self, frame):
+        for i in range(0, len(frame)):
+            frame = random.randint(0, 255)
+            pass
+        return frame
