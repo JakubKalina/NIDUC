de81f20ef1b19a34e8935e9b3634e7107ac66110
MateNaw <49524385+MateNaw@users.noreply.github.com>
2019-05-29 15:01:26 +0200

    przerobilem swoj protokol aby obslugiwal 3 mozliwe sumy oraz dodalem funckje sprawdzajaca dla bitu parzystosci

--- Receiver.py
+++ Receiver.py
@@ -1,8 +1,9 @@
 import random
 import numpy
-
+import Interference as Interfe
 
 class Receiver:
+    chosenSumAlgorithm = None
     receivedArray = []
     receivedData = []
     sending = []
@@ -10,6 +11,8 @@ class Receiver:
     frameSize = 0
     numberOfValues = 0
     xd = 0
+    numberOfPositiveSent = 0
+    numberOfNegativeSent = 0
 
     def __init__(self):
         pass
@@ -23,40 +26,55 @@ class Receiver:
         pass
 
     def receiver_frame(self):
-        print(self.receivedData)
         for i in range(0, len(self.receivedData)):
             for j in range(0, (len(self.receivedData[i]) - 1)):
                 self.receivedArray.append(self.receivedData[i][j])
         self.receivedArray = (numpy.asarray(self.receivedArray)).astype(numpy.uint8)
-        print(self.receivedArray)
+        print(f'liczba pozytywnie przeslanych pakietow: {self.numberOfPositiveSent}')
+        print(f'liczba ponownie przeslanych pakietow: {self.numberOfNegativeSent}')
+
 
     def recieve_frame(self, frame, sequence_number):
-        isGood = False
-
         # zaklocenie ramki jesli wylosuje odp warttosc
         rand = random.randrange(0, 100)
-        if rand > 99:
-            frame = self.interfere(frame)
-        checksum = self.count_sum(frame=frame)
+        if rand > 90:
+            frame = Interfe.interfere_frame(frame,self.frameSize)
+        if self.chosenSumAlgorithm == 1:
+            checksum = self.count_sum_by_Luhn(frame=frame)
+        if self.chosenSumAlgorithm == 2:
+            checksum = self.count_sum_by_CRC(frame=frame)
+        if self.chosenSumAlgorithm == 3:
+            checksum = self.count_parity_bit(frame=frame)
+
         if checksum == frame[len(frame) - 1]:
             isGood = True
         else:
             isGood = False
 
+        #potwierdzenie poprawnego odbioru lub prosba o ponowne przeslanie
         if isGood:
-            # time.sleep(2)
             # zakłócenie potwierdzenia odbioru
             randix = random.randrange(0, 100)
             if randix > 90:
+                print(f'RECEIVER: Wysyłanie powiadomienia ponownego nadesłania pakietu nr "{sequence_number}"')
+                self.numberOfNegativeSent += 1
                 return False
             else:
                 print(f'RECEIVER:odebrano ramkę nr "{sequence_number}"')
                 self.receivedData[sequence_number] = frame
+                self.numberOfPositiveSent += 1
                 return True
         else:
-            # time.sleep(0.2)
-            print(f'RECEIVER: Wysyłanie powiadomienia ponownego nadesłania pakietu nr "{sequence_number}"')
-            return False;
+            randix = random.randrange(0, 100)
+            if randix < 90:
+                print(f'RECEIVER: Wysyłanie powiadomienia ponownego nadesłania pakietu nr "{sequence_number}"')
+                self.numberOfNegativeSent += 1
+                return False
+            else:
+                print(f'RECEIVER:odebrano ramkę nr "{sequence_number}"')
+                self.receivedData[sequence_number] = frame
+                self.numberOfPositiveSent += 1
+                return True
 
     def receive_data_go_back_n(self):
         last_good_frame = -1
@@ -64,38 +82,39 @@ class Receiver:
 
         while last_good_frame < self.numberOfFrames:
             is_frame_good = False
-            frame = self.sending[last_good_frame+1]
-            received_sequence_number = self.senfingSEQ[last_good_frame+1]
+            frame = self.sending[last_good_frame + 1]
+            received_sequence_number = self.senfingSEQ[last_good_frame + 1]
 
             # zakłócenie ramki jeśli wylosuje odp wartość
             rand = random.randrange(0, 100)
             if rand > 90:
                 frame = self.interfere(frame)
-            checksum = self.count_sum(frame)
+            checksum = self.count_sum_by_Luhn(frame)
 
             if checksum == frame[len(frame) - 1]:
                 is_frame_good = True
             else:
                 is_frame_good = False
 
-            if is_frame_good and last_sequence_number+1 == received_sequence_number:
+            if is_frame_good and last_sequence_number + 1 == received_sequence_number:
                 # time.sleep(2)
                 # zakłócenie potwierdzenia odbioru
                 randix = random.randrange(0, 100)
                 if randix > 90:
-                    self.sender.ACK[last_good_frame+1] = False
+                    self.sender.ACK[last_good_frame + 1] = False
                 else:
-                    print("received pocket = " + str(last_sequence_number+1))
-                    self.receivedData[last_sequence_number+1] = frame
-                    self.sender.ACK[last_good_frame+1] = True
+                    print("received pocket = " + str(last_sequence_number + 1))
+                    self.receivedData[last_sequence_number + 1] = frame
+                    self.sender.ACK[last_good_frame + 1] = True
                     last_good_frame += 1
                     last_sequence_number += 1
             else:
                 # time.sleep(0.2)
-                print("you need to resend pocket = " + str(last_sequence_number+1))
+                print("you need to resend pocket = " + str(last_sequence_number + 1))
                 self.sender.ACK[last_good_frame + 1] = False
 
         # Metoda odbierająca dla protokołu stop-and-wait
+
     def receive_frame_stop_and_wait(self, frame, sequenceNumber):
         receivedFrameLength = len(frame)
         # Otrzymana suma kontrolna
@@ -104,8 +123,8 @@ class Receiver:
         # zaklocenie ramki jesli wylosuje odp warttosc
         rand = random.randrange(0, 100)
         if rand > 90:
-            frame = self.interfere(frame)
-        checksum = self.count_sum(frame)
+            frame = Interfe.interfere_frame(frame)
+        checksum = self.count_sum_by_Luhn(frame)
 
         if checksum == receivedSum:
             self.receivedData[sequenceNumber] = frame
@@ -113,7 +132,7 @@ class Receiver:
         else:
             return False
 
-    def count_sum(self, frame):
+    def count_sum_by_Luhn(self, frame):
         sum = 0
         for k in range(0, len(frame) - 1):
             help = frame[k]
@@ -125,12 +144,19 @@ class Receiver:
         sum = 10 - sum
         return sum
 
+    def count_sum_by_CRC(self,frame):
+        pass
 
-    def interfere(self, frame):
-        table = []
+    def count_parity_bit(self, frame):
+        count_1 = 0
         for i in range(0, len(frame) - 1):
-            table.append(random.randint(0, 256))
-        table.append(int(frame[(len(frame) - 1)]))
-        return table
-
-
+            helper = frame[i]
+            while helper > 0:
+                pom = helper % 2
+                helper = int(helper / 2)
+                if pom == 1:
+                    count_1 += 1
+        if count_1 % 2 == 1:
+            return 1
+        else:
+            return 0
